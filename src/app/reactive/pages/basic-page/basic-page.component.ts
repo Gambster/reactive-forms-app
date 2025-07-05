e98a0b8760e560051ceb2b2e3e19ce79d6a24bd7
GAMBSTER <gamboa040200@gmail.com>
2025-07-05 01:37:27 -0600

    feat: integrate form utilities for enhanced validation and error handling in basic and dynamic pages

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -6,6 +6,7 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-basic-page',
@@ -14,6 +15,7 @@ import {
 })
 export class BasicPageComponent {
   private fb = inject(FormBuilder);
+  formUtils = FormUtils;
 
   myForm: FormGroup = this.fb.group({
     name: ['', [Validators.required, Validators.minLength(3)]],
@@ -21,26 +23,17 @@ export class BasicPageComponent {
     inStorage: [0, [Validators.required, Validators.min(0)]],
   });
 
-  isValidField(fieldName: string): boolean | null {
-    return !!this.myForm.controls[fieldName].errors;
-  }
-
-  getFieldError(fieldName: string): string | null {
-    if (!this.myForm.controls[fieldName]) return null;
-
-    const errors = this.myForm.controls[fieldName].errors ?? {};
-
-    for (const key of Object.keys(errors)) {
-      switch (key) {
-        case 'required':
-          return 'Este campo es requerido';
-        case 'minlength':
-          return `Mínimo de ${errors['minlength'].requiredLength} caracteres`;
-        case 'min':
-          return `Valor mínimo de ${errors['min'].min}`;
-      }
+  onSubmit() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
     }
 
-    return null;
+    console.log(this.myForm.value);
+
+    this.myForm.reset({
+      price: 0,
+      inStorage: 0,
+    });
   }
 }
