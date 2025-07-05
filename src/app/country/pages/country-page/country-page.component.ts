b2df5adc7792ef79d6b0f8367a80ce4aee31fef1
GAMBSTER <gamboa040200@gmail.com>
2025-07-05 13:23:17 -0600

    feat: implement dynamic country selection based on region and fetch country borders

--- src/app/country/pages/country-page/country-page.component.ts
+++ src/app/country/pages/country-page/country-page.component.ts
@@ -1,8 +1,9 @@
 import { JsonPipe } from '@angular/common';
-import { Component, inject, signal } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CountryService } from '../../services/country.service';
 import { Country } from '../../interfaces/country.interfaces';
+import { filter, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-country-page',
@@ -22,4 +23,50 @@ export class CountryPageComponent {
     country: ['', Validators.required],
     border: ['', Validators.required],
   });
+
+  onFormChanged = effect((onCleanup) => {
+    const regionSubscription = this.onRegionChanged();
+    const countrySubscription = this.onCountryChanged();
+
+    onCleanup(() => {
+      regionSubscription.unsubscribe();
+      countrySubscription.unsubscribe();
+    });
+  });
+
+  onRegionChanged() {
+    return this.myForm
+      .get('region')!
+      .valueChanges.pipe(
+        tap(() => this.myForm.get('country')!.setValue('')),
+        tap(() => this.myForm.get('border')!.setValue('')),
+        tap(() => this.myForm.get('border')!.setValue('')),
+        tap(() => {
+          this.borders.set([]);
+          this.countriesByRegion.set([]);
+        }),
+        switchMap((region) => this.countryService.getCountriesByRegion(region!))
+      )
+      .subscribe((countries) => {
+        this.countriesByRegion.set(countries);
+      });
+  }
+
+  onCountryChanged() {
+    return this.myForm
+      .get('country')!
+      .valueChanges.pipe(
+        tap(() => this.myForm.get('border')!.setValue('')),
+        filter((value) => value!.length > 0),
+        switchMap((alphaCode) =>
+          this.countryService.getCountryByAlphaCode(alphaCode!)
+        ),
+        switchMap((country) =>
+          this.countryService.getCountryNamesByCodeArray(country.borders)
+        )
+      )
+      .subscribe((borders) => {
+        console.log({ borders });
+      });
+  }
 }
--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { combineLatest, Observable, of } from 'rxjs';
 import { Country } from '../interfaces/country.interfaces';
 
 @Injectable({
@@ -28,7 +28,16 @@ export class CountryService {
     return this.http.get<Country>(url);
   }
 
-  getCountryBorderByCodes(borders: string[]) {
-    //TODO: por hacer
+  getCountryNamesByCodeArray(countryCodes: string[]): Observable<Country[]> {
+    if (!countryCodes || countryCodes.length === 0) return of([]);
+
+    const countriesRequests: Observable<Country>[] = [];
+
+    countryCodes.forEach((code) => {
+      const request = this.getCountryByAlphaCode(code);
+      countriesRequests.push(request);
+    });
+
+    return combineLatest(countriesRequests);
   }
 }
