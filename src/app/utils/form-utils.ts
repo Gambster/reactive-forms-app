f4a73d1caee475db8aeaf30aca71acb9763fced0
GAMBSTER <gamboa040200@gmail.com>
2025-07-05 03:26:58 -0600

    feat: enhance registration form validation with name and email patterns, and confirm password check

--- src/app/utils/form-utils.ts
+++ src/app/utils/form-utils.ts
@@ -1,6 +1,15 @@
-import { FormArray, FormGroup, ValidationErrors } from '@angular/forms';
+import {
+  AbstractControl,
+  FormArray,
+  FormGroup,
+  ValidationErrors,
+} from '@angular/forms';
 
 export class FormUtils {
+  static namePattern = '([a-zA-Z]+) ([a-zA-Z]+)';
+  static emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
+  static notOnlySpacesPattern = '^[a-zA-Z0-9]+$';
+
   static isValidField(form: FormGroup, fieldName: string): boolean | null {
     return (
       !!form.controls[fieldName].errors && form.controls[fieldName].touched
@@ -44,8 +53,30 @@ export class FormUtils {
           return `Valor mínimo de ${errors['min'].min}`;
         case 'email':
           return 'El valor ingresado no es un correo electrónico';
+
+        case 'pattern':
+          if (errors['pattern'].requiredPattern === FormUtils.emailPattern) {
+            return 'El valor ingresado no luce como un correo electrónico';
+          }
+          return 'Error de patrón contra expresión regular';
+
+        default:
+          return `Error de validación no controlado ${key}`;
       }
     }
     return null;
+  }
+
+  static isFieldOneEqualFieldTwo(field: string, field2: string) {
+    return (formGroup: AbstractControl) => {
+      const field1Value = formGroup.get(field)?.value;
+      const field2Value = formGroup.get(field2)?.value;
+
+      return field1Value === field2Value
+        ? null
+        : {
+            passwordsNotEqual: true,
+          };
+    };
   }
 }
